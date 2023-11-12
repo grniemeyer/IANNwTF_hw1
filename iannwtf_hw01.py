214392f7223589c09f7f4cd45f6a680d8bac2c3c
grniemeyer <grniemeyer@uni-osnabrueck.de>
2023-11-12 22:06:40 +0100

    until MPL class checkkkk :D

--- iannwtf_hw01.py
+++ iannwtf_hw01.py
@@ -5,6 +5,7 @@ from sklearn.utils import shuffle
 
 # do not need to follow script that closely! ?
 
+''' 2.1 Data '''
 
 digits = load_digits()
 #print(digits.data.shape) #(1797, 64) -> already fine?
@@ -48,8 +49,12 @@ target = np.array(target)
 
 
 def minibatches_generator(data, targets, minibatchsize):
-    ''' generator function that shuffles data-target pairs
-    and creates minibatches (subsets of training data) of size minibatchsize '''
+    '''
+    Generator function that shuffles data-target pairs,
+    and creates minibatches (subsets of training data) of size minibatchsize.
+    Remark: so far the minibatchsize needs to be a factor of len(data) otherwise there are leftover data samples
+    
+    '''
     # shuffle data-target pairs
     data, targets = shuffle(data, targets)
     # create nr of minibatches with nr of samples in data and roughly minibatchsize (bc of floor division)
@@ -76,7 +81,116 @@ generator = minibatches_generator(data, target, 8)
 minibatch_data, minibatch_targets = next(generator)
 
 # Print minibatch and shapes of minibatch
-print(minibatch_data, minibatch_targets)
-print(minibatch_data.shape)
-print(minibatch_targets.shape)
+# print(minibatch_data, minibatch_targets)
+# print(minibatch_data.shape)
+# print(minibatch_targets.shape)
+
+
+
+''' 2.2 Sigmoid Activation Function '''
+
+class SigmoidActivation:
+    def __call__(self, x: np.ndarray):
+        self.x: np.ndarray = x
+
+        return 1 / (1 + np.exp(-x))
+
+
+
+''' 2.3 Softmax activation function '''
+
+class SoftmaxActivation:
+    def __call__(self, x):
+        # try different way of checking input parameter
+        assert isinstance(x, np.ndarray)
+        assert len(x.shape) == 2 # 2D array
+        assert x.shape[1] == 10 # 10 units in the second dimension
+
+        exp_x = np.exp(x - np.max(x, axis=1, keepdims=True))  # subtracting max for numerical stability?
+
+        return exp_x / np.sum(exp_x, axis=1, keepdims=True)
+    
+
+
+''' 2.4 MLP weights '''
+
+class Single_MLP_Layer:
+    def __init__(self, activation_function, n_units, input_size):
+        """
+        - activation_function: SigmoidActivation or SoftmaxActivation
+        - n_units: nr of units (perceptrons) in this layer
+        - input_size: nr of units in the preceding layer
+
+        """
+        self.activation_function = activation_function
+        self.num_units = n_units
+        self.input_size = input_size
+
+        # initialize weights and bias
+        self.weights = np.random.normal(loc=0.0, scale=0.2, size=(input_size, n_units))
+        self.bias = np.zeros(n_units)
+
+
+    def __call__(self, x):
+        # forward pass
+        self.x = x
+        assert isinstance(x, np.ndarray) 
+        assert x.shape[1] == self.input_size
+        return self.activation_function(self.x @ self.weights + self.bias)
+
+
+
+""" 2.5 Putting together the MLP """
+
+class MLP:
+    def __init__(self, layer_sizes, activation_functions):
+        """
+        - layer_sizes: list of ints representing nr of units in each layer
+        - activation_functions: list of activation function classes for each layer
+
+        """
+        #assert len(layer_sizes) == len(activation_functions)
+        #assert len(layer_sizes) > 1
+
+        self.layers = []
+
+        # create MLP layers
+        for i in range(1, len(layer_sizes)):
+            layer = Single_MLP_Layer(activation_functions[i - 1], layer_sizes[i], layer_sizes[i - 1])
+            self.layers.append(layer)
+
+
+    def __call__(self, x):
+        
+        """
+        - x: array of shape (minibatchsize, 64)
+        - output: array of shape (minibatchsize, 10)
+
+        """
+        assert isinstance(x, np.ndarray)
+        assert x.shape[1] == self.layers[0].input_size, f"Input size must be {self.layers[0].input_size}"
+
+        # forward pass through each layer
+        for layer in self.layers:
+            x = layer(x)
+
+        return x
+        
+# Inbetween check a full pass:
+minibatch_size = 32
+input_size = 64
+output_size = 10
+
+# Define layer sizes and activation functions
+layer_sizes = [input_size, 128, 64, output_size]
+activation_functions = [SigmoidActivation(), SigmoidActivation(), SoftmaxActivation()]
+
+# Create an MLP
+mlp = MLP(layer_sizes, activation_functions)
+
+# Assuming minibatch_data has shape (minibatch_size, input_size)
+output = mlp(minibatch_data)
+
+# Print the output shape
+print("MLP Output Shape:", output.shape)
 
